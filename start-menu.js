929b50ff493d51830faf16ddd192cfe0a11e7fcb
radioanakbangka <radiobudakbangka@gmail.com>
2016-11-13 04:36:40 +0700

    Add files via upload

---
+++ start-menu.js
@@ -0,0 +1,15 @@
+﻿//-------start-menu.js--------
+//-------santa-mars.blogspot.com--------
+
+//------------ Star Menu ------------------
+/* <![CDATA[ */
+document.write('<div class="mta_bar bar_logo" style="position:absolute;top:30%;">'
++'<center>'
++'<a id="home-windows"><img class="star" src="http://1.bp.blogspot.com/-PO5FCtZL-rY/URNvEwdG3MI/AAAAAAAACAs/6z3muxb_sDs/s1600/home-icon2.png" title="Welcome to my blog"/></a><br/>'
++'<a href="#buku"><img class="star" src="http://4.bp.blogspot.com/-DMLt0Y95elE/T1Ru07ScmlI/AAAAAAAABUQ/sD5wx12l1xc/s1600/mydocument.png" title="Chat"/></a><br/>'
++'<a id="start-baru"><img class="star" src="http://4.bp.blogspot.com/-e8EqPj15VGM/T0DMmN7MT7I/AAAAAAAABMs/bd11bBMQSMw/s1600/Start+Button4.png" title="Click here to begin"/></a><br/>'
++'<a href="#profil-saya"><img class="star" src="http://2.bp.blogspot.com/-GfcQ8QCp42Q/UGVtNNmlZkI/AAAAAAAAAuo/2LtCHBYZYDU/s1600/profil-saya+lah.png" title="Profil Saya"/></a><br/>'
++'<a href="#google-script"><img class="star" src="http://img1.blogblog.com/img/icon18_wrench_allbkg.png" title="Created by Santa Mars" width="28"/></a>'
++'</center>');
+document.write('</div>');
+/* ]]> */
