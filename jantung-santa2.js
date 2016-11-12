e65900ed92f1bee6a40b8d5aea4b6e80082a3e63
radioanakbangka <radiobudakbangka@gmail.com>
2016-11-13 04:40:46 +0700

    Add files via upload

---
+++ jantung-santa2.js
@@ -0,0 +1,26 @@
+﻿//-------jantung-santa2.js--------
+//-------santa-mars.blogspot.com--------
+
+//------------ Jantung Santa --------------------------------------
+/* <![CDATA[ */
+document.write('<div id="credit-ngisor">'
++'<div class="welkiri">Start</div>'
++'<div class="welkanan"> '
++'<div class="welteks">'
++'<div class="welteks1">'+''+Nama_blog_anda+''+'</div>'
++'<div class="welteks2"><span onclick="shutdown()">Shutdown</span></div></div>'
++'<div class="welfoto"><div class="gambar-blog"/><a href="/"><img width="40" class="gambar-blog" src="'+''+Icon_blog_anda+''+'" title="'+''+Nama_blog_anda+''+'"/></a></div>'
++'</div>'
++'</div>');
+document.write('</div>');
+document.write('<div id="da-vinci-carousel">'
++'<a class="mindah-jantung" href="javascript:void(0)" onclick="$(\'tabs-outer,.header-outer,.main-outer\').slideDown(3500);$(\'#jantung-santa\').fadeOut(\'slow\');" title="Lihat Posting"><img class="cloudcarousel" src="http://2.bp.blogspot.com/-V61wKG_1dKk/URNrpfP9vVI/AAAAAAAAB_Q/K-Kq0JF_2Lw/s150/posting-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="'+''+Url_twitter_anda+''+'" title="Twitter"><img class="cloudcarousel" src="http://4.bp.blogspot.com/-TKzTJCcq1ic/URNrtVUgTSI/AAAAAAAAB_o/8r6TJ6iQx2c/s150/twitter-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="'+''+Url_facebook_anda+''+'" title="Facebook"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-y8KCj3YcgSw/URNrnEDrLLI/AAAAAAAAB_A/J-mqO1tuTvE/s150/facebook-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="#profil-saya" title="Profil Saya"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-T0aPgEdF8Kc/URNrsLeEEtI/AAAAAAAAB_g/bNjBhlH0G3g/s150/profil-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="#buku" title="Chat"><img class="cloudcarousel" src="http://2.bp.blogspot.com/-SoFacoFf0hM/URNrt_WiMJI/AAAAAAAAB_w/Cd4q_hrE3M4/s150/chat-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="http://www.blogger.com/follow-blog.g?blogID='+''+ID_blog_anda+''+'" title="Ikuti Blog ini"><img class="cloudcarousel" src="http://2.bp.blogspot.com/-_95uqov6JGs/URNrovg3k1I/AAAAAAAAB_I/CCD44Iw1QL4/s150/follow-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="'+''+Url_animasi_foto_anda+''+'" title="Foto Saya"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-BCwRXlonncI/URNrqYnqDoI/AAAAAAAAB_Y/TlMfTW78a5s/s150/photo-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="'+''+Url_sitemap_anda+''+'" title="Site Map"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-5kN2EeFzbwM/URNrt-1XrFI/AAAAAAAAB_0/qVeNZlypcR8/s150/sitemap-windows-sm.jpg"/></a>');
+document.write('</div>');
+/* ]]> */
